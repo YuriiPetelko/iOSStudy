84e1e2f9f3541937bd2149d77937bae41e65b208
Oleksa Korin <oleksandr.korin@idapgroup.com>
2015-06-08 19:34:07 +0300

    project -> initial commit

---
+++ iOSProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSProject
+//
+//  Created by Oleksa Korin on 6/8/15.
+//  Copyright (c) 2015 Oleksa Korin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSProject
+//
+//  Created by Oleksa Korin on 6/8/15.
+//  Copyright (c) 2015 Oleksa Korin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
