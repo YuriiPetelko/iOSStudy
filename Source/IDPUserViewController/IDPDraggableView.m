365b08f7f9f632023d84bc40268d042c3980763b
Oleksa Korin <oleksandr.korin@idapgroup.com>
2015-06-10 19:54:49 +0300

    Manual touch handling

--- Source/IDPUserViewController/IDPDraggableView.m
+++ Source/IDPUserViewController/IDPDraggableView.m
@@ -8,6 +8,9 @@
 
 #import "IDPDraggableView.h"
 
+static NSUInteger IDPTouchCount     = 1;
+static NSTimeInterval IDPDuration   = 1.0;
+
 @interface IDPDraggableView ()
 @property (nonatomic, retain)   UITouch *leadingTouch;
 
@@ -23,6 +26,7 @@
 - (void)touchesBegan:(NSSet *)touches
            withEvent:(UIEvent *)event
 {
+    NSLog(@"began");
     self.leadingTouch = [touches anyObject];
 
     [self processTouches:touches];
@@ -31,12 +35,14 @@
 - (void)touchesMoved:(NSSet *)touches
            withEvent:(UIEvent *)event
 {
+    NSLog(@"moved");
     [self processTouches:touches];
 }
 
 - (void)touchesEnded:(NSSet *)touches
            withEvent:(UIEvent *)event
 {
+    NSLog(@"ended");
     [self processTouches:touches];
 
     self.leadingTouch = nil;
@@ -45,6 +51,7 @@
 - (void)touchesCancelled:(NSSet *)touches
                withEvent:(UIEvent *)event
 {
+    NSLog(@"cancelled");
     [self processTouches:touches];
     
     self.leadingTouch = nil;
@@ -54,16 +61,32 @@
 #pragma mark Private
 
 - (void)processTouches:(NSSet *)touches {
-    if ([touches count] != 2) {
+    if ([touches count] != IDPTouchCount) {
         return;
     }
     
     UITouch *touch = self.leadingTouch;
-    CGPoint location = [touch locationInView:self];
+    CGPoint location = [touch locationInView:self.superview];
     
+    CGFloat random = (CGFloat)arc4random() / UINT32_MAX;
+    CGFloat scale = random;
+    NSLog(@"%f", scale);
+    
+    CGAffineTransform transform = CGAffineTransformIdentity;
+    transform = CGAffineTransformConcat(transform, CGAffineTransformMakeScale(scale, scale));
+    transform = CGAffineTransformConcat(transform, CGAffineTransformMakeRotation(random * 2 * M_PI));
+
     CGRect frame = self.frame;
     frame.origin = location;
-    self.frame = frame;
+
+    [UIView animateWithDuration:IDPDuration
+                          delay:0
+                        options:UIViewAnimationOptionBeginFromCurrentState
+                     animations:^{
+//                         self.frame = frame;
+                         self.transform = transform;
+                     }
+                     completion:nil];
 }
 
 @end
