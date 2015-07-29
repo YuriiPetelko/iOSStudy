fd5e01575f5acc63130a72221d28d2d1de06afd2
Oleksa Korin <oleksandr.korin@idapgroup.com>
2015-07-29 21:14:58 +0300

    Added relation to users

--- iOSProjectTests/Specs/IDPActiveRecordSpec/IDPActiveRecordSpec.m
+++ iOSProjectTests/Specs/IDPActiveRecordSpec/IDPActiveRecordSpec.m
@@ -55,8 +55,8 @@ NSArray *(^fetchUsersFromCustomContext)(void) = ^{
     return [customContext executeFetchRequest:request error:&error];
 };
 
-describe(@"NSManagedObject", ^{
-    context(@"when inserting into NSManagedObjectContext", ^{
+describe(@"DBUser", ^{
+    context(@"when inserted into NSManagedObjectContext", ^{
         beforeAll(^{
             createUsers();
         });
@@ -85,21 +85,23 @@ describe(@"NSManagedObject", ^{
             });
             
             context(@"when fetching users from current context and custom context", ^{
+                __block NSArray *customContextUsers = nil;
+                __block NSArray *currentContextUsers = nil;
+                
+                beforeAll(^{
+                    customContextUsers = fetchUsersFromCustomContext();
+                    currentContextUsers = [DBUser fetchEntityWithSortDescriptors:nil
+                                                                       predicate:nil
+                                                                   prefetchPaths:nil];
+                });
+                
                 it(@"objects should differ", ^{
-                    NSArray *customContextUsers = fetchUsersFromCustomContext();
-                    NSArray *currentContextUsers = [DBUser fetchEntityWithSortDescriptors:nil
-                                                                                predicate:nil
-                                                                            prefetchPaths:nil];
-                    
                     BOOL value = YES;
                     NSUInteger count = 0;
                     
                     for (NSUInteger index = 0; index < entityCount; index++) {
                         id currentObject = currentContextUsers[index];
-                        [currentObject name];
-                        
                         id customObject = customContextUsers[index];
-                        NSLog(@"%@", [customObject name]);
                         
                         BOOL isEqual = currentObject == customObject;
                         value = value && isEqual;
@@ -109,6 +111,91 @@ describe(@"NSManagedObject", ^{
                     [[theValue(count) shouldNot] equal:theValue(entityCount)];
                     [[theValue(value) should] equal:theValue(NO)];
                 });
+                
+                it(@"objects ID's should equal", ^{
+                    BOOL value = YES;
+                    NSUInteger count = 0;
+                    
+                    for (NSUInteger index = 0; index < entityCount; index++) {
+                        id currentObject = currentContextUsers[index];
+                        id customObject = customContextUsers[index];
+                        
+                        BOOL isEqual = [[currentObject objectID] isEqualToId:[customObject objectID]];
+                        value = value && isEqual;
+                        count += isEqual ? 1 : 0;
+                    }
+                    
+                    [[theValue(count) should] equal:theValue(entityCount)];
+                    [[theValue(value) should] equal:theValue(YES)];
+                });
+            });
+        });
+        
+        context(@"when adding DBImage", ^{
+            __block DBUser *user = nil;
+            
+            beforeEach(^{
+                user = [[DBUser fetchEntityWithSortDescriptors:nil
+                                                     predicate:nil
+                                                 prefetchPaths:nil] firstObject];
+            });
+            
+            afterEach(^{
+                [NSManagedObjectContext resetManagedObjectContext];
+            });
+            
+            it(@"image should contain it, when added in direct relation", ^{
+                DBImage *image = [DBImage managedObject];
+                [user addImage:image];
+                
+                [[image.user should] equal:user];
+            });
+            
+            it(@"it should contain image, when added in inverse relation", ^{
+                DBImage *image = [DBImage managedObject];
+                image.user = user;
+                
+                [[user.images should] contain:image];
+            });
+        });
+        
+        context(@"when adding friends", ^{
+            __block DBUser *user = nil;
+            __block NSArray *users = nil;
+            
+            beforeEach(^{
+                users = [DBUser fetchEntityWithSortDescriptors:nil
+                                                     predicate:nil
+                                                 prefetchPaths:nil];
+                user = [users firstObject];
+                
+                for (DBUser *entity in users) {
+                    entity.friends = [NSSet setWithArray:users];
+                }
+                
+                NSLog(@"%@", users);
+            });
+            
+            afterEach(^{
+                [NSManagedObjectContext resetManagedObjectContext];
+            });
+            
+            it(@"its friends count should equal all users count", ^{
+                [[user.friends should] haveCountOf:[users count]];
+            });
+            
+            it(@"it should contain all users in DB", ^{
+                [[user.friends should] equal:[NSSet setWithArray:users]];
+            });
+            
+            it(@"it should delete all users, when friend relation is cascade deleted", ^{
+                [user deleteManagedObject];
+                
+                NSArray *usersAfterDelete = [DBUser fetchEntityWithSortDescriptors:nil
+                                                                         predicate:nil
+                                                                     prefetchPaths:nil];
+                
+                [[usersAfterDelete should] haveCountOf:0];
             });
         });
     });
